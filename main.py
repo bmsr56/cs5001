c5fd656255bcc4aeaaf30ae9a52faca1f2b37fd9
ben <bms56@mst.edu>
2018-12-12 18:52:18 -0600

    Skeletons of functions coming along

--- main.py
+++ main.py
@@ -2,16 +2,65 @@
 # Ben Simpson
 # 12-12-2018
 
+import numpy as np
+
+class ValueIterationLearner():
+    def __init__():
+        self.gamma = 0.8
+
+    def printValues(values):
+        print('+-------+-------+-------+-------+-------+-------+-------+-------+')
+        for i in values:
+                print('|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|'.format(i[0],i[1],i[2] ,i[3],i[4],i[5],i[6],i[7]))
+        print('+-------+-------+-------+-------+-------+-------+-------+-------+\n')
+        return
+
+    def Prob(iterations):
+        """ 
+        """
+        return
+
+    def Value(iterations):
+        """ 
+        """
+        return
+
+    def ExpReward(iterations):
+        """ 
+        """
+        return
+
+    def Reward(iterations):
+        """ 
+        """
+        return
+
+    def Reward(iterations):
+        """ 
+        """
+        return
+
+    def GetPolicy(location):
+        """ Accepts: <r,c> location tuple
+            Returns: action
+        """
+
+
+
 def printValues(values):
-   print('+-------+-------+-------+-------+-------+-------+-------+-------+')
-   for i in values:
-        print('|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|{}\t|'.format(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7]))
-   print('+-------+-------+-------+-------+-------+-------+-------+-------+\n')
 
 def main():
-    values = [i for i in range(10)]
-    printValues(values)
+    gamma = 0.8 # discount rate
+    Qnext = [[[0 for k in range(4)] for j in range(10)] for i in range(10)]
+    Qprev = [[[0 for k in range(4)] for j in range(10)] for i in range(10)]
+    count = 0
+    userInput = input()
+    # capture input
+    while input > 0
+
+
+    print(Qprev)
     return
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
